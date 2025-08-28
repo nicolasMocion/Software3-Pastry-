4c77a3a19ddb10e948c3f330bdcfbe5006ca4802
omar56663313 <nicolas.osobustos@gmail.com>
2025-08-28 00:20:39 -0500

    Database - node conection done; Angular implemented in the project; Angular - node conection midway

--- backend-gateway/src/app.js
+++ backend-gateway/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { pool } from './config/db.js';
+import productsRoutes from './routes/products.js';
 
 dotenv.config();
 
@@ -11,20 +12,19 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Pastry API Gateway is running!');
+// Routes
+app.use('/api/products', productsRoutes);
+
+// Test DB connection
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('DB connection error:', err);
+  } else {
+    console.log('DB connection successful:', res.rows[0]);
+  }
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-
-
-pool.query('SELECT NOW()', (err, res) => {
-    if (err) {
-      console.error('DB connection error', err);
-    } else {
-      console.log('DB connection successful:', res.rows[0]);
-    }
-  });
-  
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
